c2f6821aebe19530fbfb0aad674f4ebfb1d8f036
neshdev999 <71468718+neshdev999@users.noreply.github.com>
2020-09-29 18:21:43 -0400

    5-commit-Pizza-stores-gotolink-added
    pizza store buttons with click event added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Javascript code goes here
+
+function pizzaClickButtonEvent() {
+
+    document.getElementById('papa').onclick = function() {
+        location.href = 'https://www.papajohns.com';
+    };
+
+    document.getElementById('domino').onclick = function() {
+        location.href = 'https://www.dominos.com';
+    };
+
+    document.getElementById('pizzaHut').onclick = function() {
+        location.href = 'https://www.pizzahut.com/';
+    };
+
+}
