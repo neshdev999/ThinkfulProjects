8fc589ac424e6bdc811b90251d9b3d0f50e6a446
neshdev999 <71468718+neshdev999@users.noreply.github.com>
2020-09-30 14:11:07 -0400

    8-commit-fix-javascript-bug
    Fixed bug which require to double click the button to go to required pizza store

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
 // Javascript code goes here
 
-function pizzaClickButtonEvent() {
-
-    let papaButton = document.getElementById('papa');
-    let dominoButton = document.getElementById('domino');
-    let pizzaHutButton = document.getElementById('pizzaHut');
-
-    papaButton.addEventListener("click", function() {
+function pizzaClickButtonEvent(id) {
+    if (id === "papa") {
         location.href = 'https://www.papajohns.com';
-    });
-
-    dominoButton.addEventListener("click", function() {
+    } else if (id === "domino") {
         location.href = 'https://www.dominos.com';
-    });
-
-    pizzaHutButton.addEventListener("click", function() {
+    } else if (id === "pizzaHut") {
         location.href = 'https://www.pizzahut.com/';
-    });
-
-
-
-    // document.getElementById('papa').onclick = function() {
-    //     location.href = 'https://www.papajohns.com';
-    // };
-
-    // document.getElementById('domino').onclick = function() {
-    //     location.href = 'https://www.dominos.com';
-    // };
-
-    // document.getElementById('pizzaHut').onclick = function() {
-    //     location.href = 'https://www.pizzahut.com/';
-    // };
-
+    } else {
+        alert("Something went wrong!!! Reclick your choice");
+    }
 }
